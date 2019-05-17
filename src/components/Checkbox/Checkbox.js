ce2bb6d11601b27661e285bf38b0878198d3c0b4
Adrian Osmond <adrian@Adrians-MacBook-Pro-2.local>
2019-05-17 11:47:53 +0200

    Use inline svg for checkbox

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,35 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import check from 'assets/images/check.svg';
 
 const Wrapper = styled.label`
   position: relative;
   padding-left: 22px;
   cursor: pointer;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 14px;
-    height: 14px;
-    margin-top: -7px;
-    background-color: #6f9d6b;
-  }
-
-  &:after {
-    content: '';
-    display: ${p => (p.isChecked ? 'block' : 'none')};
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 14px;
-    height: 14px;
-    margin-top: -7px;
-    background-image: url(${check});
-    background-size: 100%;
-  }
 `;
 
 const Input = styled.input`
@@ -40,9 +15,38 @@ const Input = styled.input`
   width: 0;
 `;
 
+const Box = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 14px;
+  height: 14px;
+  margin-top: -7px;
+  background-color: #6f9d6b;
+`;
+
+const Check = ({ isChecked }) => (
+  <Box>
+    {isChecked && (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="14"
+        height="14"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#FFF"
+          d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"
+        />
+      </svg>
+    )}
+  </Box>
+);
+
 const Checkbox = ({ label, isChecked, onChange }) => (
-  <Wrapper isChecked={isChecked}>
+  <Wrapper>
     <Input type="checkbox" value={isChecked} onChange={onChange} />
+    <Check isChecked={isChecked} />
     <span>{label}</span>
   </Wrapper>
 );
